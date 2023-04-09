d3863162fe3b1939aadbef4ead16912c659518ed
Yauhen Klimovich <webrubin1987@gmail.com>
2023-04-09 16:21:53 +0300

    feat: add overflow hidden for pets and index html

--- shelter/assets/scripts/burger.js
+++ shelter/assets/scripts/burger.js
@@ -1,7 +1,18 @@
+// const burgerIcon = document.querySelector('.burger-icon');
+// const navMenu = document.querySelector('.nav-menu');
+//
+// burgerIcon.addEventListener('click', function() {
+//     this.classList.toggle('active');
+//     navMenu.classList.toggle('active');
+// });
+//
+
+
 const burgerIcon = document.querySelector('.burger-icon');
 const navMenu = document.querySelector('.nav-menu');
 
 burgerIcon.addEventListener('click', function() {
     this.classList.toggle('active');
     navMenu.classList.toggle('active');
+    document.body.classList.toggle('menu-open'); // добавляем класс к body
 });
